fc3f013c5e58d2d5ca49edb87c1fe03647d274b6
Moltivie <major2015usa@gmail.com>
2022-11-03 15:17:11 +0100

    Remove unnecessary comments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -135,30 +135,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-// const AuthShowcase: React.FC = () => {
-//   const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery();
-
-//   const { data: sessionData } = useSession();
-
-//   return (
-//     <div className="flex flex-col items-center justify-center gap-2">
-//       {sessionData && (
-//         <p className="text-2xl text-blue-500">
-//           Logged in as {sessionData?.user?.name}
-//         </p>
-//       )}
-//       {secretMessage && (
-//         <p className="text-2xl text-blue-500">{secretMessage}</p>
-//       )}
-//       <button
-//         className="rounded-md border border-black bg-violet-50 px-4 py-2 text-xl shadow-lg hover:bg-violet-100"
-//         onClick={
-//           sessionData ? () => console.log(sessionData) : () => signIn("github")
-//         }
-//       >
-//         {sessionData ? "Sign out" : "Sign in"}
-//       </button>
-//     </div>
-//   );
-// };
