7111be8a42dc8c5bb81a0389d65164f74c26a132
Moltivie <major2015usa@gmail.com>
2022-11-03 03:46:50 +0100

    Use GitHub provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,5 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import GitHubProvider from "next-auth/providers/github";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
@@ -19,11 +19,10 @@ export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    GitHubProvider({
+      clientId: env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
+      clientSecret: env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET,
     }),
-    // ...add more providers here
   ],
 };
 
