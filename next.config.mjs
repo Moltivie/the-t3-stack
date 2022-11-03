fce6d1a372b6b689cc111e4a4595862cee1caa86
Moltivie <major2015usa@gmail.com>
2022-11-03 05:08:25 +0100

    Added image domains

--- next.config.mjs
+++ next.config.mjs
@@ -13,5 +13,8 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  images: {
+    domains: ["cloudflare-ipfs.com", "avatars.githubusercontent.com"],
+  },
 };
 export default config;
