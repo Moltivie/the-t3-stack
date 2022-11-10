a6231d57e8a106473837aba9518d46a28ef378b5
Moltivie <major2015usa@gmail.com>
2022-11-10 22:24:33 +0100

    Modified index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,32 +4,8 @@ import Link from "next/link";
 
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
-import { trpc } from "../utils/trpc";
 
-import { AiOutlinePlus } from "react-icons/ai";
-import { ImSpinner2 } from "react-icons/im";
-
-import { useRouter } from "next/router";
-
-const Home: NextPage = () => {
-  const router = useRouter();
-
-  const { data: dataUsers, isLoading: isLoadingUsers } =
-    trpc.users.getAll.useQuery();
-
-  // Get the mutation to create a user
-  const createUserMutation = trpc.users.createUser.useMutation();
-
-  const handleCreate = async () => {
-    // Create a user
-    const hasBeenCreated = await createUserMutation.mutateAsync();
-
-    // If the user has been created, refresh the page
-    if (hasBeenCreated) {
-      router.reload();
-    }
-  };
-
+const Index: NextPage = () => {
   // Handle the session
   const { data: session } = useSession();
 
@@ -46,7 +22,7 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>List Users</title>
+        <title>Home</title>
         <meta name="description" content="" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -55,7 +31,7 @@ const Home: NextPage = () => {
         <section className="mx-auto flex max-w-7xl flex-col items-center justify-center">
           <header className="grid w-full grid-cols-3 items-center justify-center py-10">
             <h1 className="col-start-2 text-center text-4xl font-bold uppercase tracking-wider text-gray-200">
-              List of Users
+              The Doom
             </h1>
             {
               // If the user is not logged in, show the login button.
@@ -101,68 +77,56 @@ const Home: NextPage = () => {
               )
             }
           </header>
-          <div className="w-full p-2 lg:p-0">
-            {isLoadingUsers && (
-              <Image
-                className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform"
-                src="/pulse.svg"
-                alt="Loading..."
-                width={200}
-                height={200}
-              />
-            )}
-            {!isLoadingUsers && (
-              <ul className="grid grid-cols-3 items-center justify-center gap-4 lg:grid-cols-4">
-                {dataUsers?.map((user) => (
-                  <Link key={user.id} href={`/getdetails/${user.id}`}>
-                    <li
-                      key={user.id}
-                      className="group my-2 flex w-full cursor-pointer items-center gap-5 rounded-md bg-gray-700 p-4 transition duration-100 hover:scale-105 hover:bg-gray-600"
+          <div className="grid w-full grid-cols-2">
+            <div className="flex items-center justify-center">
+              <div className="w-2/3 text-left text-gray-200">
+                <h1 className="text-6xl font-bold tracking-wide">
+                  Candidate as a moderator
+                </h1>
+                <p className="mt-8 text-2xl">
+                  Do you feel like you have a lot to offer to the community? Let
+                  us know!
+                </p>
+                <div className="mt-6 flex w-full justify-start space-x-4">
+                  <Link href="/candidate/moderator">
+                    <button
+                      type="button"
+                      className="transform rounded-md border border-gray-200 bg-gray-200 py-2 px-4 text-2xl font-semibold tracking-wide text-gray-700 transition duration-100 hover:scale-105"
                     >
-                      <Image
-                        className="transtition rounded-full border-2 border-gray-200 duration-300 group-hover:scale-125"
-                        src={user.avatar}
-                        width={50}
-                        height={50}
-                        alt={`avatar-${user.id}`}
-                      />
-                      <p className="w-full text-end text-2xl text-gray-200">
-                        {user.name}
-                      </p>
-                    </li>
+                      Candidate
+                    </button>
                   </Link>
-                ))}
-                {session && (session.user?.role || 1) > 1 && (
+                </div>
+              </div>
+            </div>
+            <div className="flex h-96 flex-col items-center">
+              <div className="w-2/3 text-right text-gray-200">
+                <h1 className="mt-10 text-6xl font-bold tracking-wide">
+                  Register as a voter
+                </h1>
+                <p className="mt-8 text-2xl">
+                  Register as a voter to vote your favorite user!
+                </p>
+                <div className="mt-4 flex w-full justify-end space-x-4">
                   <button
                     type="button"
-                    onClick={handleCreate}
-                    disabled={createUserMutation.isLoading}
+                    className="rounded-md border border-gray-200 bg-transparent p-2 font-semibold text-gray-200 transition duration-200 hover:bg-gray-200 hover:text-gray-800"
+                    // skipcq: JS-0417
                   >
-                    <li
-                      key="add-user"
-                      className={`my-2 flex h-20 w-full border-spacing-y-32 cursor-pointer items-center justify-center gap-5 rounded-md border-2 border-dashed bg-transparent p-4 transition duration-100 hover:scale-105 hover:bg-gray-600 ${
-                        createUserMutation.isLoading &&
-                        "pointer-events-none opacity-30"
-                      }`}
-                    >
-                      {createUserMutation.isLoading ? (
-                        <ImSpinner2 className="animate-spin text-3xl text-gray-200" />
-                      ) : (
-                        <AiOutlinePlus className="text-3xl text-gray-200" />
-                      )}
-                    </li>
+                    Sign in
                   </button>
-                )}
-              </ul>
-            )}
+                  <button
+                    type="button"
+                    className="transform rounded-md border border-gray-200 bg-gray-200 p-2 font-semibold text-gray-700 transition duration-200 hover:scale-105"
+                  >
+                    Sign Up
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
-        <footer
-          className={`
-            ${
-              !isLoadingUsers ? "mt-10 " : "absolute bottom-0 w-full "
-            } grid grid-cols-3 p-4`}
-        >
+        <footer className="absolute bottom-0 grid w-full grid-cols-3 p-4">
           <p className="col-start-2 flex justify-center capitalize text-gray-200">
             Made with ❤️ by Aiman El Aaqdi
           </p>
@@ -181,4 +145,4 @@ const Home: NextPage = () => {
   );
 };
 
-export default Home;
+export default Index;
