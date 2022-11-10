49c1657e4c65b0ec58e03f0b7f52bc875e161c3e
Moltivie <major2015usa@gmail.com>
2022-11-10 22:23:51 +0100

    Added register moderator api routes

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,12 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
 import { usersRouter } from "./users";
+import { registerModeratorRouter } from "./register.moderator";
 
 export const appRouter = router({
   users: usersRouter,
   auth: authRouter,
+  registerModerator: registerModeratorRouter,
 });
 
 // export type definition of API
---
+++ src/server/trpc/router/register.moderator.ts
@@ -0,0 +1,26 @@
+import { publicProcedure, router } from "../trpc";
+
+export const registerModeratorRouter = router({
+  checkUser: publicProcedure.query(async ({ ctx }) => {
+    const hasalredyRegistered = await ctx.prisma.moderator.findFirst({
+      where: {
+        userId: ctx.session?.user?.id,
+      },
+    });
+
+    return hasalredyRegistered;
+  }),
+  registerUser: publicProcedure.mutation(async ({ ctx }) => {
+    const updateModerator = await ctx.prisma.moderator.upsert({
+      create: {
+        userId: ctx.session?.user?.id!,
+        updatedAt: new Date(),
+      },
+      update: {},
+      where: {
+        userId: ctx.session?.user?.id!,
+      },
+    });
+    return updateModerator;
+  }),
+});
